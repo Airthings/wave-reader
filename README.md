22191faec289bf263f96deadaea12a5289f7647c
orjangje <orjan.gjengedal@airthings.com>
2018-12-13 10:47:48 +0100

    First release. Added read_wave.py and README.

---
+++ README.md
@@ -0,0 +1,178 @@
+# Airthings Wave Sensor Reader
+
+This is a project to provide users an interface (```read_wave.py```) to read current sensor values from the
+[Airthings Wave](https://airthings.com/wave/) devices using a Raspberry Pi 3 Model B over Bluetooth Low Energy (BLE).
+
+Airthings Wave is a smart Radon detector, including sensors for temperature and humidity.
+Additionally, you can simply wave in front of the device to get a visual indication of your radon levels.
+
+**Table of contents**
+
+* [Requirements](#requirements)
+  * [Setup Raspberry Pi](#setup-Raspberry-pi)
+  * [Turn on the BLE interface](#turn-on-the-ble-interface)
+  * [Installing linux and python packages](#installing-linux-and-python-packages)
+  * [Downloading script](#downloading-script)
+* [Usage](#usage)
+  * [Printing data to the terminal window](#printing-data-to-the-terminal-window)
+  * [Piping data to a text-file](#piping-data-to-a-text-file)
+* [Sensor data description](#sensor-data-description)
+* [Release notes](#release-notes)  
+
+# Requirements
+
+The following tables shows a compact overview of dependencies for this project.
+
+**List of OS dependencies**
+
+| OS | model/version | Comments |
+|-------------|-------------|-------------|
+| Raspberry Pi 3 | Model B | Raspbian image
+| Linux          | x86 Debian | Should work according to [bluepy](https://github.com/IanHarvey/bluepy)
+
+**List of linux/raspberry dependencies**
+
+| package | version | Comments |
+|-------------|-------------|-------------|
+| python  | 2.7 | Tested with python 2.7.13
+| python-pip | | pip for python2.7
+| git        | | To download this project
+| libglib2.0-dev  | | For bluepy module
+
+**List of Python dependencies**
+
+| module | version | Comments |
+|-------------|-------------|-------------|
+| bluepy      | 1.2.0 | Newer versions have not been tested.
+| tableprint  | 0.8.0 | Newer versions have not been tested.
+
+## Setup Raspberry Pi
+
+The first step is to setup the Raspberry Pi with Raspbian. An installation guide for 
+Raspbian can be found on the [Raspberry Pi website](https://www.raspberrypi.org/downloads/raspbian/).
+In short: download the Raspbian image and write it to a micro SD card.
+
+To continue, you need access to the Raspberry Pi using either a monitor and keyboard, or 
+by connecting through WiFi or ethernet from another computer. The latter option does not 
+require an external screen or keyboard and is called “headless” setup. To access a headless 
+setup, you must first activate SSH on the Pi. This can be done by creating a file named ssh 
+in the boot partition of the SD card. Connect to the Pi using SSH from a command line 
+interface (terminal):
+
+```
+$ ssh pi@raspberrypi.local
+```
+
+The default password for the “pi” user is “raspberry”.
+
+## Turn on the BLE interface
+
+In the terminal window on you Raspberry Pi:
+
+```
+pi@raspberrypi:~$ bluetoothctl
+[bluetooth]# power on
+[bluetooth]# show
+```
+
+After issuing the command ```show```, a list of bluetooth settings will be printed
+to the Raspberry Pi terminal window. Look for “Powered: yes”.
+
+## Installing linux and python packages
+
+> Note: The ```read_wave.py``` script is only compatible with Python2.7.
+
+The next step is to install the bluepy Python library for talking to the BLE stack. 
+For the current released version for Python 2.7:
+
+```
+pi@raspberrypi:~$ sudo apt-get install python-pip libglib2.0-dev
+pi@raspberrypi:~$ sudo pip2 install bluepy==1.2.0
+```
+
+Make sure your Raspberry Pi has git installed
+
+```
+pi@raspberrypi:~$ git --version
+```
+
+or install git to be able to clone this repo.
+
+```
+pi@raspberrypi:~$ sudo apt-get install git
+```
+
+Additionally, the ```read_wave.py``` script depends on the ```tableprint``` module
+to print nicely formated sensor data to the Raspberry Pi terminal at run-time.
+
+```
+pi@raspberrypi:~$ sudo pip2 install tableprint==0.8.0
+```
+
+> Note: The ```read_wave.py``` script has been tested with bluepy==1.2.0 and tableprint==0.8.0. You may download the latest versions at your own risk.
+
+## Downloading script
+
+Downloading using git:
+
+```
+pi@raspberrypi:~$ sudo git clone https://github.com/Airthings/wave.git
+```
+
+Downloading using wget:
+
+```
+pi@raspberrypi:~$ wget https://raw.githubusercontent.com/Airthings/wave/master/read_wave.py
+```
+
+# Usage
+
+To read the sensor data from the Airthings Wave Plus using the ```read_wave.py``` script,
+you need the 10-digit serial number of the device. This can be found under the magnetic backplate 
+of your Airthings Wave Plus.
+
+If your device is paired and connected to e.g. a phone, you may need to turn off bluetooth on
+your phone while using this script.
+
+```cd``` into the directory where the read_wave.py script is located if you cloned the repo.
+
+```
+pi@raspberrypi:~$ cd wave
+```
+
+## Printing data to the terminal window
+
+Run the Python script ```read_wave.py``` in the following way:
+
+```
+pi@raspberrypi:~/wave $ sudo python2 read_wave.py [SN] terminal
+```
+
+where you change [SN] with the 10-digit serial number. 
+
+After a short delay, the script will print the current sensor values to the 
+Raspberry Pi terminal window. Exit the script using “Ctrl + c”.
+
+## Piping data to a text-file
+
+If you want to pipe the results to a text-file, you can run the script in the following way:
+
+```
+pi@raspberrypi:~/wave $ sudo python2 read_wave.py [SN] pipe > yourfile.txt
+```
+
+where you change [SN] with the 10-digit serial number. Exit the script using “Ctrl + c”.
+
+# Sensor data description
+
+| sensor | units | Comments |
+|-------------|-------------|-------------|
+| Datetime                      | YYYY-MM-DD HH:MM:SS | 
+| Humidity                      | %rH | 
+| Temperature                   | &deg;C | 
+| Radon short term average      | Bq/m3 | First measurement available 1 hour after inserting batteries
+| Radon long term average       | Bq/m3 | First measurement available 1 hour after inserting batteries
+
+# Release notes
+
+Initial release 13-Dec-2018
---
+++ read_wave.py
@@ -0,0 +1,211 @@
+# MIT License
+#
+# Copyright (c) 2018 Airthings AS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# https://airthings.com
+
+# ===============================
+# Module import dependencies
+# ===============================
+
+from bluepy.btle import UUID, Peripheral, Scanner, DefaultDelegate
+import sys
+from datetime import datetime
+import time
+import struct
+import tableprint
+
+# ===============================
+# Script guards for correct usage
+# ===============================
+
+if len(sys.argv) < 3:
+    print "ERROR: Missing input argument SN or MODE."
+    print "USAGE: read_waveplus.py [SN] [MODE]"
+    print "    where [SN] is the 10-digit serial number found under the magnetic backplate of your waveplus."
+    print "    where [MODE] is either terminal or pipe."
+    sys.exit(1)
+elif str(sys.argv[1]).isdigit() is not True or len(sys.argv[1]) != 10:
+    print "ERROR: Invalid SN format."
+    print "USAGE: read_waveplus.py \"SN\" \"MODE\""
+    print "    where [SN] is the 10-digit serial number found under the magnetic backplate of your waveplus."
+    print "    where [MODE] is either terminal or pipe."
+    sys.exit(1)
+elif sys.argv[2].lower()!='terminal' and sys.argv[2].lower()!='pipe':
+    print "ERROR: Invalid MODE argument."
+    print "USAGE: read_waveplus.py \"SN\" \"MODE\""
+    print "    where [SN] is the 10-digit serial number found under the magnetic backplate of your waveplus."
+    print "    where [MODE] is either terminal or pipe."
+    sys.exit(1)
+else:
+    SerialNumber = int(sys.argv[1])
+    Mode         = sys.argv[2].lower()
+
+# ====================================
+# Utility functions for WavePlus class
+# ====================================
+
+def parseSerialNumber(ManuDataHexStr):
+    if (ManuDataHexStr == "None"):
+        SN = "Unknown"
+    else:
+        ManuData = bytearray.fromhex(ManuDataHexStr)
+
+        if (((ManuData[1] << 8) | ManuData[0]) == 0x0334):
+            SN  =  ManuData[2]
+            SN |= (ManuData[3] << 8)
+            SN |= (ManuData[4] << 16)
+            SN |= (ManuData[5] << 24)
+        else:
+            SN = "Unknown"
+    return SN
+
+# ===================================
+# Sensor index definitions
+# ===================================
+
+SENSOR_IDX_DATETIME      = 0
+SENSOR_IDX_HUMIDITY      = 1
+SENSOR_IDX_TEMPERATURE   = 2
+SENSOR_IDX_RADON_ST_AVG  = 3
+SENSOR_IDX_RADON_LT_AVG  = 4
+
+# ===============================
+# Class WavePlus
+# ===============================
+
+class Wave():
+
+    UUID_DATETIME     = UUID(0x2A08)
+    UUID_HUMIDITY     = UUID(0x2A6F)
+    UUID_TEMPERATURE  = UUID(0x2A6E)
+    UUID_RADON_ST_AVG = UUID("b42e01aa-ade7-11e4-89d3-123b93f75cba")
+    UUID_RADON_LT_AVG = UUID("b42e0a4c-ade7-11e4-89d3-123b93f75cba")
+
+    def __init__(self, SerialNumber):
+        self.periph            = None
+        self.datetime_char     = None
+        self.humidity_char     = None
+        self.temperature_char  = None
+        self.radon_st_avg_char = None
+        self.radon_lt_avg_char = None
+
+    def connect(self):
+        scanner     = Scanner().withDelegate(DefaultDelegate())
+        deviceFound = False
+        searchCount = 0
+        while deviceFound is False and searchCount < 50:
+            devices      = scanner.scan(0.1) # 0.1 seconds scan period
+            searchCount += 1
+            for dev in devices:
+                ManuData = dev.getValueText(255)
+                SN = parseSerialNumber(ManuData)
+                if (SN == SerialNumber):
+                    MacAddr = dev.addr
+                    deviceFound  = True # exits the while loop on next conditional check
+                    break # exit for loop
+        
+        if (deviceFound is not True):
+            print "ERROR: Could not find device."
+            print "GUIDE: (1) Please verify the serial number. (2) Ensure that the device is advertising. (3) Retry connection."
+            sys.exit(1)
+        else:
+            self.periph = Peripheral(MacAddr)
+            self.datetime_char     = self.periph.getCharacteristics(uuid=self.UUID_DATETIME)[0]
+            self.humidity_char     = self.periph.getCharacteristics(uuid=self.UUID_HUMIDITY)[0]
+            self.temperature_char  = self.periph.getCharacteristics(uuid=self.UUID_TEMPERATURE)[0]
+            self.radon_st_avg_char = self.periph.getCharacteristics(uuid=self.UUID_RADON_ST_AVG)[0]
+            self.radon_lt_avg_char = self.periph.getCharacteristics(uuid=self.UUID_RADON_LT_AVG)[0]
+            
+    def read(self, sensor_idx):
+        if (sensor_idx==SENSOR_IDX_DATETIME and self.datetime_char!=None):
+                rawdata = self.datetime_char.read()
+                rawdata = struct.unpack('HBBBBB', rawdata)
+                data    = datetime(rawdata[0], rawdata[1], rawdata[2], rawdata[3], rawdata[4], rawdata[5])
+                unit    = " "
+        elif (sensor_idx==SENSOR_IDX_HUMIDITY and self.humidity_char!=None):
+                rawdata = self.humidity_char.read()
+                data    = struct.unpack('H', rawdata)[0] * 1.0/100.0
+                unit    = " %rH"
+        elif (sensor_idx==SENSOR_IDX_TEMPERATURE and self.temperature_char!=None):
+                rawdata = self.temperature_char.read()
+                data    = struct.unpack('h', rawdata)[0] * 1.0/100.0
+                unit    = " degC"
+        elif (sensor_idx==SENSOR_IDX_RADON_ST_AVG and self.radon_st_avg_char!=None):
+                rawdata = self.radon_st_avg_char.read()
+                data    = struct.unpack('H', rawdata)[0] * 1.0
+                unit    = " Bq/m3"
+        elif (sensor_idx==SENSOR_IDX_RADON_LT_AVG and self.radon_lt_avg_char!=None):
+                rawdata = self.radon_lt_avg_char.read()
+                data    = struct.unpack('H', rawdata)[0] * 1.0
+                unit    = " Bq/m3"
+        else:
+            print "ERROR: Unkown sensor ID or device not paired"
+            print "GUIDE: (1) method connect() must be called first, (2) Ensure correct sensor_idx input value."
+            sys.exit(1)
+        return str(data)+unit
+    
+    def disconnect(self):
+        if self.periph is not None:
+            self.periph.disconnect()
+            self.periph            = None
+            self.datetime_char     = None
+            self.humidity_char     = None
+            self.temperature_char  = None
+            self.radon_st_avg_char = None
+            self.radon_lt_avg_char = None
+
+try:
+    #---- Connect to device ----#
+    wave = Wave(SerialNumber)
+    wave.connect()
+    
+    if (Mode=='terminal'):
+        print "Press ctrl-C to exit program"
+    
+    header = ['Datetime', 'Humidity', 'Temperature', 'Radon ST avg', 'Radon LT avg']
+    
+    if (Mode=='terminal'):
+        print tableprint.header(header, width=20)
+    elif (Mode=='pipe'):
+        print header
+
+    while True:
+        
+        # read current values
+        date_time    = wave.read(SENSOR_IDX_DATETIME)
+        humidity     = wave.read(SENSOR_IDX_HUMIDITY)
+        temperature  = wave.read(SENSOR_IDX_TEMPERATURE)
+        radon_st_avg = wave.read(SENSOR_IDX_RADON_ST_AVG)
+        radon_lt_avg = wave.read(SENSOR_IDX_RADON_LT_AVG)
+        
+        data = [date_time, humidity, temperature, radon_st_avg, radon_lt_avg]
+        
+        # Print data
+        if (Mode=='terminal'):
+            print tableprint.row(data, width=20)
+        elif (Mode=='pipe'):
+            print data
+            
+        time.sleep(2.0)
+            
+finally:
+    wave.disconnect()
